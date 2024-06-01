b9ed6089a594858f0a6606c54c383fd4196fd8fa
Md Zahid Hussain <134547438+CallmeZahid@users.noreply.github.com>
2024-06-01 14:36:10 +0530

    Update game.js

--- game.js
+++ game.js
@@ -69,7 +69,7 @@ getNewQuestion = () => {
     if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS){
         localStorage.setItem("mostRecentScore", score);
         /*go to the end page*/
-        return window.location.assign("../pages/end.html");
+        return window.location.assign("end.html");
     }
 
     questionCounter++;
